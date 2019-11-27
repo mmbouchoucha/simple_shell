e0c2e5d3405a013f261d563b52c739991e35d928
mmbouchoucha <mehdiboch@gmail.com>
2019-11-27 23:09:14 +0000

    update04

--- shell.h
+++ shell.h
@@ -36,5 +36,22 @@
 
 /* _getline.c */
 ssize_t _getline(char **lineptr, size_t *n, int fd, list_t **mt);
+void reset_buffer(char *buffer, ssize_t buf_size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size, list_t **mt);
+char *_strdup(char *str, list_t **mt);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strtok(char *s, const char *delim);
+unsigned int _strlen(char *s);
 
+/* string02.c */
+int _strcmp(char *s1, char *s2);
+int _atoi(char *s);
+char *_strcat(char *s1, char *s2, list_t **mt);
+char *_itoa(ssize_t num, list_t **mt);
+char *reverse_str(char *s);
+
+/* string03.c */
+char *_strchr(char *s, char c);
+void _chartostr(char *s, char c);
 #endif /* _SHELL_H_ */
